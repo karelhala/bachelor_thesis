7cf3a099837c51013ffea1bba397268795b1c63f
Karel Hala <ch4rlien@gmail.com>
2013-11-20 14:37:41 +0100

    Create UCPlaceModel.java

---
+++ source/BT/src/BT/managers/UC/UCPlaceModel.java
@@ -0,0 +1,18 @@
+/*
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+*/
+package BT.managers.UC;
+
+import BT.modules.UC.places.UCUseCase;
+import BT.modules.UC.places.UCJoinEdge;
+import BT.modules.UC.places.UCActor;
+import BT.models.CoordinateModel;
+import java.util.ArrayList;
+
+/**
+*
+* @author Karel Hala
+*/
+public class UCPlaceModel {
+}
