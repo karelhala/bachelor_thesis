4d9e4ac12491e151e9328093fed7c6c976517c3a
Karel Hala <ch4rlien@gmail.com>
2013-11-21 11:50:22 +0100

    refs #2 create parent class LeftTopContent

---
+++ source/BT/src/BT/models/LeftTopContent.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BT.models;
+
+import javax.swing.JPanel;
+
+**
+ * Parent class, for ContentPane
+ * @author Karel Hala
+ */
+public final class ContentPaneModel {
+    /**
+     * @var Jpanel for storing every component of top left content 
+     */
+    protected JPanel mainContentPane;
+
+    
+    /**
+     * Method, that returns main pane has content of left top pane
+     * @return JPanel left top pane of application
+     */
+    public JPanel getMainContentPane ()
+    {
+        return this.mainContentPane;
+    }
+    
+    /**
+     * Method, that returns main pane has content of left top pane
+     * @return JPanel left top pane of application
+     */
+    public void setMainContentPane (Jpanel mainContentPane)
+    {
+        this.mainContentPane = mainContentPane;
+    }
+}
