75c2d17bbb9309b44f238d73dc3fc78ec947b2db
ch4rlien <ch4rlien@gmail.com>
2014-04-03 02:23:25 +0200

    Add checking of classes that are connected. Add javadoc for myArrayList.

--- source/BT/src/BT/models/MyArrayList.java
+++ source/BT/src/BT/models/MyArrayList.java
@@ -9,12 +9,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
+ * Class that extends array list by few methods
  *
  * @author Karel
  * @param <E>
  */
 public class MyArrayList<E> extends ArrayList<E> {
 
+    /**
+     * method that will swap whole array by middle element
+     */
     public void swapWholeArrayList() {
         if (!this.isEmpty()) {
             for (int i = 0; i <= (this.size() - 1) / 2; i++) {
@@ -23,6 +27,12 @@ public class MyArrayList<E> extends ArrayList<E> {
         }
     }
 
+    /**
+     * Method for returning first elemnt. It will return element at position 0
+     * if arrayList is not empty
+     *
+     * @return element at first position
+     */
     public E getFirst() {
         if (!this.isEmpty()) {
             return this.get(0);
@@ -30,6 +40,12 @@ public class MyArrayList<E> extends ArrayList<E> {
         return null;
     }
 
+    /**
+     * Method for returning last elemnt. It will return element at position
+     * arrayList.size()-1 if arrayList is not empty
+     *
+     * @return element at last position
+     */
     public E getLast() {
         if (!this.isEmpty()) {
             return this.get(this.size() - 1);
@@ -37,6 +53,13 @@ public class MyArrayList<E> extends ArrayList<E> {
         return null;
     }
 
+    /**
+     * Method for returning left muiddle element. It will return element at
+     * position arrayList.size()/2-1 if arrayList is not empty. If size of
+     * arrayList is bellow or equal 2 return first elemnt.
+     *
+     * @return element at left middle position
+     */
     public E getLeftMiddle() {
         if (!this.isEmpty()) {
             if (this.size() <= 2) {
@@ -47,10 +70,17 @@ public class MyArrayList<E> extends ArrayList<E> {
         return null;
     }
 
+    /**
+     * Method for returning right muiddle element. It will return element at
+     * position arrayList.size()/2 if arrayList is not empty. It will return
+     * this elemnt only if size of arrayList is bigger than 1
+     *
+     * @return element at right middle position
+     */
     public E getRightMiddle() {
         if (!this.isEmpty()) {
             if (this.size() > 1) {
-                return this.get(this.size() / 2 - 1 + 1);
+                return this.get(this.size() / 2);
             }
         }
         return null;
--- source/BT/src/BT/modules/ClassDiagram/mainContent/CDJoinEdgeManipulator.java
+++ source/BT/src/BT/modules/ClassDiagram/mainContent/CDJoinEdgeManipulator.java
@@ -5,26 +5,23 @@
  */
 package BT.modules.ClassDiagram.mainContent;
 
-import BT.BT;
 import BT.BT.CDLineType;
 import BT.BT.ClassType;
 import BT.models.CoordinateModel;
+import BT.models.MyArrayList;
 import BT.modules.ClassDiagram.places.CDClass;
 import BT.modules.ClassDiagram.places.joinEdge.CDJoinEdgeController;
+import javax.swing.JOptionPane;
 import javax.swing.JToggleButton;
 
 /**
- *
+ * Class for manipulating with join edges in class diagram part.
  * @author Karel
  */
 public class CDJoinEdgeManipulator {
 
-    public CDJoinEdgeManipulator() {
-        super();
-    }
-
     /**
-     *
+     * Method for changing  type of join edge based on name of button
      * @param selectedButton
      * @param joinEdge
      */
@@ -49,9 +46,10 @@ public class CDJoinEdgeManipulator {
     }
 
     /**
-     *
-     * @param joinEdge
-     * @param clickedObject
+     * Method for creating join edge bwtween classes. It will create new join edge if no one exists
+     * and add to it on first or second place new object. Based on which part is epmty.
+     * @param joinEdge edge that is being created
+     * @param clickedObject object that is inserted either on first or second place in join edge.
      * @return
      */
     public static CDJoinEdgeController createJoinEdge(CDJoinEdgeController joinEdge, CoordinateModel clickedObject) {
@@ -68,23 +66,81 @@ public class CDJoinEdgeManipulator {
         return joinEdge;
     }
     
+    /**
+     * Method for checking objects and lines between them.
+     * If you are joining interfaces, you can use only generalization. If you connect
+     * to any class interface you can do it only with realization.
+     * @param joinEdge that is under inspection
+     * @return 
+     */
     public static CDJoinEdgeController checkObjects(CDJoinEdgeController joinEdge)
     {
+        String errorMessage = "";
         try
         {
+            //check for connection between interface and any other class
             if (((CDClass)joinEdge.getFirstObject()).getTypeOfClass() == ClassType.INTERFACE && joinEdge.getJoinEdgeType() != CDLineType.GENERALIZATION && 
                    joinEdge.getSecondObject() != null && ((CDClass)joinEdge.getSecondObject()).getTypeOfClass() == ClassType.INTERFACE)
             {
+                errorMessage = "You can't use any other join edge between interface and other classes than GENERALIZATION.";
                 joinEdge.setFirstObject(null);
                 joinEdge.setSecondObject(null);
             }
+            
+            //check for connection between any class and interface
             if (((CDClass)joinEdge.getFirstObject()).getTypeOfClass() != ClassType.INTERFACE && joinEdge.getSecondObject() != null && ((CDClass)joinEdge.getSecondObject()).getTypeOfClass() == ClassType.INTERFACE &&
                     joinEdge.getJoinEdgeType() != CDLineType.REALIZATION)
             {
+                errorMessage = "You can't connect any other class with  interface, than with REALIZATION join.";
                 joinEdge.setSecondObject(null);
             }
-        }catch (NullPointerException exep){
             
+            //check for connection bewteen activity and actor
+            if (((CDClass)joinEdge.getFirstObject()).getTypeOfClass() == ClassType.ACTIVITY)
+            {
+                if (joinEdge.getSecondObject() != null)
+                {
+                    if (((CDClass)joinEdge.getSecondObject()).getTypeOfClass() == ClassType.ACTOR)
+                    {
+                        if (joinEdge.getJoinEdgeType() != CDLineType.ASSOCIATION)
+                        {
+                            errorMessage = "You can't use any other join between actor and action than ASSOCIATON.";
+                            joinEdge.setSecondObject(null);
+                        }
+                        else
+                        {
+                            CoordinateModel firstObject = joinEdge.getFirstObject();
+                            joinEdge.setFirstObject(joinEdge.getSecondObject());
+                            joinEdge.setSecondObject(firstObject);
+                            ((MyArrayList)joinEdge.getBreakPoints()).swapWholeArrayList();
+                        }
+                    }
+                }
+            }
+            
+            //check for connection bewteen actor and activity
+            if (((CDClass)joinEdge.getFirstObject()).getTypeOfClass() == ClassType.ACTOR)
+            {
+                if (joinEdge.getSecondObject() != null)
+                {
+                    if (((CDClass)joinEdge.getSecondObject()).getTypeOfClass() == ClassType.ACTIVITY)
+                    {
+                        if (joinEdge.getJoinEdgeType() != CDLineType.ASSOCIATION)
+                        {
+                            errorMessage = "You can't use any other join between actor and action than ASSOCIATON.";
+                            joinEdge.setSecondObject(null);
+                        }
+                    }
+                }
+            }
+        }catch (NullPointerException exep){
+            System.err.println(exep.getMessage());
+        }
+        
+        //display error message if needed
+        if (!errorMessage.equals(""))
+        {
+            JOptionPane.showMessageDialog(null, errorMessage);
         }
         return joinEdge;
     }
