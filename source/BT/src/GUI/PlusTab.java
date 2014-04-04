c0bf88f2b285008b1f68d818a0d715b239c5f2c3
Karel Hala <karel.hala@honeywell.com>
2014-04-04 10:53:39 +0200

    Change plus tab. if no icon is provided, add + string instead of it. Add comentaries to plus tab

--- source/BT/src/GUI/PlusTab.java
+++ source/BT/src/GUI/PlusTab.java
@@ -14,63 +14,72 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 /**
- *
+ * Class that creates plus tab at end of tabs in tabbed pane.
  * @author Karel Hala
  */
 public class PlusTab {
 
+    /**
+     * Button that is plus button.
+     */
     private JButton buttonPlus;
 
     /**
-     *
-     * @param tabPane
+     * Method that creates pane for plus button, insert this button in it and insert this panel in tab Pane.
+     * @param tabPane tab pane that holds this plus button
      */
     public void AddPlusTab(JTabbedPane tabPane) {
         JPanel closePanel = new JPanel(new BorderLayout());
         closePanel.setOpaque(false);
-        tabPane.addTab(null, null);
+        closePanel.add(createPlusButton(), BorderLayout.CENTER);
+	tabPane.addTab(null, null);
         tabPane.setBorder(null);
         int index = tabPane.getTabCount() - 1;
-        this.buttonPlus = createPlusButton();
-        closePanel.add(this.buttonPlus, BorderLayout.CENTER);
         tabPane.setTabComponentAt(index, closePanel);
         tabPane.setEnabledAt(index, false);
         tabPane.setSelectedIndex(index - 1);
     }
 
     /**
-     *
-     * @return
+     * Method for creating initializing and creating new plus button.
+     * @return JButton with icon as plus button.
      */
     private JButton createPlusButton() {
-        JButton plusButton = new JButton();
-
-        plusButton.setBorder(null);
-        plusButton.setFocusable(false);
-        plusButton.setContentAreaFilled(false);
-        createIconsForButton(plusButton);
-        return plusButton;
+        this.buttonPlus = new JButton();
+        this.buttonPlus.setBorder(null);
+        this.buttonPlus.setFocusable(false);
+        this.buttonPlus.setContentAreaFilled(false);
+	this.buttonPlus.setToolTipText("Add new tab");
+        createIconsForButton();
+        return this.buttonPlus;
     }
 
     /**
-     *
-     * @param button
+     * Method for creating icon for plus button.
      */
-    private void createIconsForButton(JButton button) {
-        ImageIcon icon = new ImageIcon(CloseTabbedPane.class.getResource("/resources/greenPlus.png"));
-        Image normalImage = icon.getImage();
-        Image grayImage = GrayFilter.createDisabledImage(normalImage);
-        ImageIcon greyIcon = new ImageIcon(grayImage);
+    private void createIconsForButton() {
+	try
+	{
+	    ImageIcon icon = new ImageIcon(CloseTabbedPane.class.getResource("/resources/greenPlus.png"));
+	    Image normalImage = icon.getImage();
+	    Image grayImage = GrayFilter.createDisabledImage(normalImage);
+	    ImageIcon greyIcon = new ImageIcon(grayImage);
 
-        button.setRolloverEnabled(true);
-        button.setContentAreaFilled(false);
-        button.setRolloverIcon(icon);
-        button.setIcon(greyIcon);
+	    this.buttonPlus.setRolloverEnabled(true);
+	    this.buttonPlus.setContentAreaFilled(false);
+	    this.buttonPlus.setRolloverIcon(icon);
+	    this.buttonPlus.setIcon(greyIcon);
+	}
+	catch(NullPointerException exep)
+	{
+	    System.err.println(exep.getMessage());
+	    this.buttonPlus.setText("+");
+	}
     }
 
     /**
-     *
-     * @param addNewTabListener
+     * Method for adding mouse listener to plus button.
+     * @param addNewTabListener that will be triggered on button clicked.
      */
     public void addMouseClickedListenerToPlus(MouseAdapter addNewTabListener) {
         this.buttonPlus.addMouseListener(addNewTabListener);
