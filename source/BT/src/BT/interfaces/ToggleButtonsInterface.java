f0e7714132031a3e60d7df7baab614dd7d7cac89
Karel Hala <karel.hala@honeywell.com>
2014-04-04 09:47:47 +0200

    Add comentaries to togglebuttons interface

--- source/BT/src/BT/interfaces/ToggleButtonsInterface.java
+++ source/BT/src/BT/interfaces/ToggleButtonsInterface.java
@@ -7,12 +7,21 @@ package BT.interfaces;
 import javax.swing.JToggleButton;
 
 /**
- *
+ * Class for creating toggle buttons located in pane.
  * @author Karel Hala
  */
 public interface ToggleButtonsInterface {
 
+    /**
+     * Method for fetching selected button.
+     * @return JtoggleButton that is selected.
+     */
     public JToggleButton getSelectedButton();
 
+    /**
+     * Method for fetching button with desired name.
+     * @param name of button to be returned.
+     * @return JtoggleButton with name.
+     */
     public JToggleButton getButtonWithName(String name);
 }
