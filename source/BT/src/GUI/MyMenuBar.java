8d76d95c9a20db60245843221d8e6f0d3a45ecf7
Karel Hala <karel.hala@honeywell.com>
2014-04-04 12:17:40 +0200

    Add items to menu bar

--- source/BT/src/GUI/MyMenuBar.java
+++ source/BT/src/GUI/MyMenuBar.java
@@ -4,19 +4,22 @@
  */
 package GUI;
 
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 /**
  *
  * @author Karel Hala
  */
-public class MyMenuBar {
-
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
+public class MyMenuBar extends MyMenyBarModel{
 
     public MyMenuBar() {
         initializeMenu();
         initializeItems();
+	addItemsToFilemenu();
+	addItemsToEditMenu();
+	addItesmToExportMenu();
         addItemsToMenu();
     }
 
@@ -24,35 +27,77 @@ public class MyMenuBar {
      *
      */
     private void initializeMenu() {
-        this.jMenuBar1 = new javax.swing.JMenuBar();
+        this.jMenuBar1 = new JMenuBar();
     }
 
     /**
      *
      * @return
      */
-    public javax.swing.JMenuBar getMainMenu() {
-        javax.swing.JMenuBar menubar = this.jMenuBar1;
-        return menubar;
+    public JMenuBar getMainMenu() {
+        return this.jMenuBar1;
     }
 
     /**
      *
      */
     private void initializeItems() {
-        this.jMenu1 = new javax.swing.JMenu();
-        this.jMenu2 = new javax.swing.JMenu();
+        this.jMenu1 = new JMenu();
+        this.jMenu2 = new JMenu();
+	this.jMenu3 = new JMenu();
 
         this.jMenu1.setText("File");
         this.jMenu2.setText("Edit");
+	this.jMenu3.setText("Export");
     }
 
+    /**
+     * Method for inserting file items to file menu.
+     */
+    private void addItemsToFilemenu()
+    {
+	this.newFile = new JMenuItem("New File");
+	this.openFile = new JMenuItem("Open File");
+	this.closeFile = new JMenuItem("Close File");
+	
+	this.jMenu1.add(this.newFile);
+	this.jMenu1.add(this.openFile);
+	this.jMenu1.add(this.closeFile);
+    }
+    
+    /**
+     * Method for initializing and inserting edit menu items.
+     */
+    private void addItemsToEditMenu()
+    {
+	this.backAction = new JMenuItem("Back");
+	this.frontAction = new JMenuItem("forward");
+	
+	this.jMenu2.add(this.backAction);
+	this.jMenu2.add(this.frontAction);
+    }
+    
+    /**
+     * Method for initializing and inserting export menu items.
+     */
+    private void addItesmToExportMenu()
+    {
+	this.exportToEps = new JMenuItem("Export to EPS");
+	this.exportToXml = new JMenuItem("Export to XML");
+	this.exportToPdf = new JMenuItem("Export to PDF");
+	
+	this.jMenu3.add(this.exportToEps);
+	this.jMenu3.add(this.exportToXml);
+	this.jMenu3.add(this.exportToPdf);
+    }
+    
     /**
      *
      */
     private void addItemsToMenu() {
         this.jMenuBar1.add(this.jMenu1);
         this.jMenuBar1.add(this.jMenu2);
+	this.jMenuBar1.add(this.jMenu3);
     }
 
 }
---
+++ source/BT/src/GUI/MyMenyBarModel.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package GUI;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author E819996
+ */
+public class MyMenyBarModel {
+    protected JMenu jMenu1;
+    protected JMenu jMenu2;
+    protected JMenu jMenu3;
+    protected JMenuItem exportToPdf;
+    protected JMenuItem exportToEps;
+    protected JMenuItem exportToXml;
+    protected JMenuItem newFile;
+    protected JMenuItem closeFile;
+    protected JMenuItem openFile;
+    protected JMenuItem backAction;
+    protected JMenuItem frontAction;
+    protected JMenuBar jMenuBar1;
+
+    public JMenu getjMenu1() {
+	return jMenu1;
+    }
+
+    public JMenu getjMenu2() {
+	return jMenu2;
+    }
+
+    public JMenu getjMenu3() {
+	return jMenu3;
+    }
+
+    public JMenuItem getExportToPdf() {
+	return exportToPdf;
+    }
+
+    public JMenuItem getExportToEps() {
+	return exportToEps;
+    }
+
+    public JMenuItem getExportToXml() {
+	return exportToXml;
+    }
+
+    public JMenuItem getNewFile() {
+	return newFile;
+    }
+
+    public JMenuItem getCloseFile() {
+	return closeFile;
+    }
+
+    public JMenuItem getOpenFile() {
+	return openFile;
+    }
+
+    public JMenuItem getBackAction() {
+	return backAction;
+    }
+
+    public JMenuItem getFrontAction() {
+	return frontAction;
+    }
+
+    public JMenuBar getjMenuBar1() {
+	return jMenuBar1;
+    }
+    
+    public void setjMenu1(JMenu jMenu1) {
+	this.jMenu1 = jMenu1;
+    }
+
+    public void setjMenu2(JMenu jMenu2) {
+	this.jMenu2 = jMenu2;
+    }
+
+    public void setjMenu3(JMenu jMenu3) {
+	this.jMenu3 = jMenu3;
+    }
+
+    public void setExportToPdf(JMenuItem exportToPdf) {
+	this.exportToPdf = exportToPdf;
+    }
+
+    public void setExportToEps(JMenuItem exportToEps) {
+	this.exportToEps = exportToEps;
+    }
+
+    public void setExportToXml(JMenuItem exportToXml) {
+	this.exportToXml = exportToXml;
+    }
+
+    public void setNewFile(JMenuItem newFile) {
+	this.newFile = newFile;
+    }
+
+    public void setCloseFile(JMenuItem closeFile) {
+	this.closeFile = closeFile;
+    }
+
+    public void setOpenFile(JMenuItem openFile) {
+	this.openFile = openFile;
+    }
+
+    public void setBackAction(JMenuItem backAction) {
+	this.backAction = backAction;
+    }
+
+    public void setFrontAction(JMenuItem frontAction) {
+	this.frontAction = frontAction;
+    }
+
+    public void setjMenuBar1(JMenuBar jMenuBar1) {
+	this.jMenuBar1 = jMenuBar1;
+    }
+}
