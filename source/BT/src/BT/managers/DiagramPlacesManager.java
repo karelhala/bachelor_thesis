bfe91125cc01e7268e65ab42dfaf4e98f7756164
Karel Hala <ch4rlien@gmail.com>
2014-04-23 13:01:23 +0200

    Update DiagramPlacesManager.java
    Add commentaries to diagramPlacesManager.

--- source/BT/src/BT/managers/DiagramPlacesManager.java
+++ source/BT/src/BT/managers/DiagramPlacesManager.java
@@ -11,95 +11,149 @@ import BT.modules.ClassDiagram.places.CDClass;
 import java.util.ArrayList;
 
 /**
- *
- * @author Karel
+ * Class that holds every place manager.
+ * This class is used to store every place and operations with it.
+ * It holds number of diagram (index of tab).
+ * File name (name of opened file).
+ * UcPlaces actors and actions of useCase diagram.
+ * CdPlaces class diagram classes.
+ * PnPlaces are stored in arrayList because of methods can have petriNet too.
+ * @author Karel Hala
  */
 public class DiagramPlacesManager {
     
     /**
-     * 
+     * Name of opened file.
      */
     private String fileName;
     
     /**
-     * 
+     * Index of tab, easier access to this class.
      */
     private int diagramNumber;
     
     /**
-     * 
+     * UseCase diagram places.
      */
     private PlaceManager ucPlaces;
     
     /**
-     * 
+     * Class diagram places.
      */
     private PlaceManager cdPlaces;
     
     /**
-     * 
+     * ArrayList of petriNets diagrams.
      */
     private ArrayList<PlaceManager> pnPlaces;
 
+    /**
+     * Basic constructor for creating every placeManager.
+     */
     public DiagramPlacesManager() {
         this.ucPlaces = new PlaceManager();
         this.cdPlaces = new PlaceManager();
         this.pnPlaces = new ArrayList<>();
     }
 
+    /**
+     * Getter for class diagram places.
+     * @return PlaceManager of class diagram.
+     */
     public PlaceManager getCdPlaces() {
         return cdPlaces;
     }
 
+    /**
+     * Setter for class diagram places.
+     */
     public void setCdPlaces(PlaceManager cdPlaces) {
         this.cdPlaces = cdPlaces;
     }
 
+    /**
+     * Getter for fetching all petriNet diagrams.
+     * @return ArrayList<PlaceManager> of petriNets.
+     */
     public ArrayList<PlaceManager> getPnPlaces() {
         return pnPlaces;
     }
 
+    /**
+     * Setter for petriNet diagrams.
+     * @param pnPlaces ArrayList<PlaceManager> of petriNet diagrams.
+     */
     public void setPnPlaces(ArrayList<PlaceManager> pnPlaces) {
         this.pnPlaces = pnPlaces;
     }
 
-    public void addPnPlace(PlaceManager pnPlaceManager)
-    {
+    /**
+     * Add new petriNet diagram to file.
+     * @param pnPlaceManager PlaceManager of inserted petriNet diagram.
+     */
+    public void addPnPlace(PlaceManager pnPlaceManager) {
         this.pnPlaces.add(pnPlaceManager);
     }
-    
-    public void removePnPlace(PlaceManager pnPlaceManager)
-    {
+
+    /**
+     * Remove petriNet diagram from opened file.
+     * @param pnPlaceManager PlaceManager of deleted petriNet diagram.
+     */    
+    public void removePnPlace(PlaceManager pnPlaceManager) {
         this.pnPlaces.remove(pnPlaceManager);
     }
-    
+
+    /**
+     * Getter for fetching useCase diagram places.
+     * @return PlaceManager of useCase diagram.
+     */    
     public PlaceManager getUcPlaces() {
         return ucPlaces;
     }
 
+    /**
+     * Setter for useCase diagram places.
+     * @param ucPlaces PlaceManager of useCase diagram.
+     */
     public void setUcPlaces(PlaceManager ucPlaces) {
         this.ucPlaces = ucPlaces;
     }
 
+    /**
+     * Getter for fetching name of file.
+     * @return String name of file.
+     */
     public String getFileName() {
         return fileName;
     }
 
+    /**
+     * Setter for name of file.
+     * @param fileName name of file as String.
+     */
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
-
+    
+    /**
+     * Getter for index of diagram.
+     * @return int integer of tab.
+     */
     public int getDiagramNumber() {
         return diagramNumber;
     }
 
+    /**
+     * Setter for index of diagram.
+     * @param diagramNumber index of diagram as integer.
+     */
     public void setDiagramNumber(int diagramNumber) {
         this.diagramNumber = diagramNumber;
     }
     
     /**
      * Method for getting selected class from selected file.
-     * @return 
+     * @return CDClass if any class is selected.
      */
     public CDClass getSelectedClass()
     {
